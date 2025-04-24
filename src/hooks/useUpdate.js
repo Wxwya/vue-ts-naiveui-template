45f1394f08f9ff0d15a9c114e1f81a11e931a879
xwya <747273022@qq.com>
2025-04-24 18:35:02 +0800

    增加自动检测更新功能和打包脚本

--- src/hooks/useUpdate.js
+++ src/hooks/useUpdate.js
@@ -4,7 +4,7 @@ import { onBeforeUnmount, onMounted } from "vue";
 let version = cache.getLocalStorage(VERSION);
 let timer = null;
 let isModal= false
-const TIME_DATE = 10000
+const TIME_DATE = 1000 * 60 * 30 
 const useUpdate = () => { 
   const getVersion = async () => {
     try { 
