a865684dde0716a37493f1c99f4cbb266f6b39b5
xwya <747273022@qq.com>
2025-04-24 18:26:49 +0800

    增加自动检测更新功能和打包脚本

--- scripts/version-update.js
+++ scripts/version-update.js
@@ -1,5 +1,3 @@
-// const fs = require('fs')
-// const path = require('path')
 import { dirname, resolve} from 'path'
 import fs from 'fs'
 import { fileURLToPath } from 'url'
@@ -7,9 +5,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const pkgPath = resolve(__dirname, '../package.json')
 const publicPath = resolve(__dirname, '../public/version.json')
-// 1. 读取 package.json
 const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'))
-// 2. 自动递增 patch 版本
 let [major, minor, patch] = pkg.version.split('.').map(Number)
 patch += 1
 if (patch >= 10) {
@@ -20,10 +16,7 @@ if (patch >= 10) {
     major += 1
   }
 }
-
 const newVersion = `${major}.${minor}.${patch}`
 pkg.version = newVersion
 fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2))
-
-// 5. 写入 public/version.json
 fs.writeFileSync(publicPath, JSON.stringify({ version: newVersion }, null, 2))
